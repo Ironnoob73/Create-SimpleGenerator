aab47b8ba4d00712af4aede8a0c50c75327f6738
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-04-07 18:35:54 +0800

    Item charging
    为物品充电

--- src/main/java/dev/hail/create_simple_generator/StressGeneratorEntity.java
+++ src/main/java/dev/hail/create_simple_generator/StressGeneratorEntity.java
@@ -1,12 +1,17 @@
 package dev.hail.create_simple_generator;
 
 import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
+import com.simibubi.create.content.kinetics.belt.behaviour.TransportedItemStackHandlerBehaviour;
+import com.simibubi.create.foundation.blockEntity.behaviour.BlockEntityBehaviour;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.Container;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.HopperBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.energy.IEnergyStorage;
+import net.neoforged.neoforge.items.IItemHandler;
 
 public class StressGeneratorEntity extends KineticBlockEntity{
     public StressGeneratorEntity(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
@@ -15,14 +20,46 @@ public class StressGeneratorEntity extends KineticBlockEntity{
     @Override
     public void tick() {
         super.tick();
-        Direction face = getBlockState().getValue(StressGeneratorBlock.FACING);
-        IEnergyStorage energyStorage = null;
-        if (level != null) {
-            energyStorage = level.getCapability(Capabilities.EnergyStorage.BLOCK, getBlockPos().relative(face), face);
-        }
-        if (energyStorage == null) {
+        if (getSpeed() == 0){
             return;
         }
-        energyStorage.receiveEnergy((int) Math.abs(getSpeed() * Config.generatorGeneratesEnergyMultiplier), false);
+        Direction face = getBlockState().getValue(StressGeneratorBlock.FACING);
+        IEnergyStorage energyStorage = null;
+        Container itemStorage = null;
+        IItemHandler singleItemStorage = null;
+        boolean justExtracted;
+
+        if (level != null) {
+            energyStorage = level.getCapability(Capabilities.EnergyStorage.BLOCK, getBlockPos().relative(face), face);
+            itemStorage = HopperBlockEntity.getContainerAt(level, getBlockPos().relative(face));
+            singleItemStorage = level.getCapability(Capabilities.ItemHandler.BLOCK, getBlockPos().relative(face), face);
+        }
+        justExtracted = extractPowerTo(energyStorage);
+        if (itemStorage != null && !justExtracted) {
+            for (int i = 0; i < itemStorage.getContainerSize(); i++ ){
+                energyStorage = itemStorage.getItem(i).getCapability(Capabilities.EnergyStorage.ITEM);
+                justExtracted = extractPowerTo(energyStorage);
+                if (justExtracted){
+                    return;
+                }
+            }
+        }
+        if (singleItemStorage != null && !justExtracted){
+            for (int i = 0; i < singleItemStorage.getSlots(); i++ ){
+                energyStorage = singleItemStorage.getStackInSlot(i).getCapability(Capabilities.EnergyStorage.ITEM);
+                justExtracted = extractPowerTo(energyStorage);
+                if (justExtracted){
+                    return;
+                }
+            }
+        }
+    }
+
+    public boolean extractPowerTo (IEnergyStorage energyStorage){
+        if (energyStorage != null && (energyStorage.getEnergyStored() < energyStorage.getMaxEnergyStored() || energyStorage.getMaxEnergyStored() == 0)){
+                energyStorage.receiveEnergy((int) Math.abs(getSpeed() * Config.generatorGeneratesEnergyMultiplier), false);
+                return true;
+            }
+        return false;
     }
 }
