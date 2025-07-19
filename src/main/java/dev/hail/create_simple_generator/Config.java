aaf3debd2411399e35d7422fe256649c2f873936
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-07-19 11:08:34 +0800

    Charge Entities
    为实体充电和相关配置

--- src/main/java/dev/hail/create_simple_generator/Config.java
+++ src/main/java/dev/hail/create_simple_generator/Config.java
@@ -14,10 +14,19 @@ public class Config
             .defineInRange("generator_generates_energy_multiplier", 1D, 0D, Double.MAX_VALUE);
     private static final ModConfigSpec.DoubleValue GENERATOR_CONSUMPTION_STRESS_MULTIPLIER = BUILDER_S
             .defineInRange("generator_consumption_stress_multiplier", 1D, 0D, Double.MAX_VALUE);
+    private static final ModConfigSpec.BooleanValue CAN_CHARGE_ITEM_IN_BLOCK = BUILDER_S
+            .define("can_charge_item_in_block", true);
+    private static final ModConfigSpec.BooleanValue CAN_CHARGE_ITEM_ENTITY = BUILDER_S
+            .define("can_charge_item_entity", true);
+    private static final ModConfigSpec.BooleanValue CAN_CHARGE_ITEM_IN_ENTITY = BUILDER_S
+            .define("can_charge_item_in_entity", true);
     static final ModConfigSpec SPEC_S = BUILDER_S.build();
 
     public static double generatorGeneratesEnergyMultiplier;
     public static double generatorConsumptionStressMultiplier;
+    public static boolean canChargeItemInBlock;
+    public static boolean canChargeItemEntity;
+    public static boolean canChargeItemInEntity;
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent.Loading event)
@@ -25,6 +34,9 @@ public class Config
         if (event.getConfig().getSpec() == SPEC_S) {
             generatorGeneratesEnergyMultiplier = GENERATOR_GENERATES_ENERGY_MULTIPLIER.get();
             generatorConsumptionStressMultiplier = GENERATOR_CONSUMPTION_STRESS_MULTIPLIER.get();
+            canChargeItemInBlock = CAN_CHARGE_ITEM_IN_BLOCK.get();
+            canChargeItemEntity = CAN_CHARGE_ITEM_ENTITY.get();
+            canChargeItemInEntity = CAN_CHARGE_ITEM_IN_ENTITY.get();
         }
     }
 }
--- src/main/java/dev/hail/create_simple_generator/StressGeneratorEntity.java
+++ src/main/java/dev/hail/create_simple_generator/StressGeneratorEntity.java
@@ -4,16 +4,26 @@ import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.Container;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.HopperBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.energy.EnergyStorage;
 import net.neoforged.neoforge.energy.IEnergyStorage;
 import net.neoforged.neoforge.items.IItemHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class StressGeneratorEntity extends KineticBlockEntity{
     private final EnergyStorage energyStorage = new EnergyStorage(0);
+    protected List<Entity> caughtEntities = new ArrayList<>();
     public StressGeneratorEntity(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
         super(typeIn, pos, state);
     }
@@ -26,33 +36,73 @@ public class StressGeneratorEntity extends KineticBlockEntity{
         Direction face = getBlockState().getValue(StressGeneratorBlock.FACING);
         IEnergyStorage energyStorage = null;
         Container itemStorage = null;
-        IItemHandler singleItemStorage = null;
+        IItemHandler itemHandler = null;
         boolean justExtracted;
 
         if (level != null) {
             energyStorage = level.getCapability(Capabilities.EnergyStorage.BLOCK, getBlockPos().relative(face), face.getOpposite());
             itemStorage = HopperBlockEntity.getContainerAt(level, getBlockPos().relative(face));
-            singleItemStorage = level.getCapability(Capabilities.ItemHandler.BLOCK, getBlockPos().relative(face), face.getOpposite());
+            itemHandler = level.getCapability(Capabilities.ItemHandler.BLOCK, getBlockPos().relative(face), face.getOpposite());
         }
         justExtracted = extractPowerTo(energyStorage);
-        if (itemStorage != null && !justExtracted) {
-            for (int i = 0; i < itemStorage.getContainerSize(); i++ ){
-                energyStorage = itemStorage.getItem(i).getCapability(Capabilities.EnergyStorage.ITEM);
-                justExtracted = extractPowerTo(energyStorage);
-                if (justExtracted){
-                    return;
+        if (justExtracted){
+            return;
+        }
+        if (itemStorage != null && Config.canChargeItemInBlock && forItemInStorage(itemStorage)){
+            return;
+        }
+        if (itemHandler != null && Config.canChargeItemInBlock && forItemInStorage(itemHandler)){
+            return;
+        }
+        if (Config.canChargeItemEntity || Config.canChargeItemInEntity){
+            caughtEntities.clear();
+            caughtEntities = Objects.requireNonNull(getLevel()).getEntities(
+                    null, new AABB(getBlockPos().relative(face)).expandTowards(Vec3.atLowerCornerOf(face.getNormal()).scale(0)));
+            for (Entity entity : caughtEntities) {
+                if (entity instanceof ItemEntity itemEntity && Config.canChargeItemEntity) {
+                    energyStorage = itemEntity.getItem().getCapability(Capabilities.EnergyStorage.ITEM);
+                    justExtracted = extractPowerTo(energyStorage);
+                    if (justExtracted) {
+                        return;
+                    }
+                }
+                if (Config.canChargeItemInEntity){
+                    if (entity instanceof Player player){
+                        itemStorage = player.getInventory();
+                        if (forItemInStorage(itemStorage)){
+                            return;
+                        }
+                    }
+                    itemHandler = entity.getCapability(Capabilities.ItemHandler.ENTITY);
+                    if (itemHandler != null && forItemInStorage(itemHandler)){
+                        return;
+                    }
                 }
             }
         }
-        if (singleItemStorage != null && !justExtracted){
-            for (int i = 0; i < singleItemStorage.getSlots(); i++ ){
-                energyStorage = singleItemStorage.getStackInSlot(i).getCapability(Capabilities.EnergyStorage.ITEM);
-                justExtracted = extractPowerTo(energyStorage);
-                if (justExtracted){
-                    return;
-                }
+    }
+
+    public boolean forItemInStorage (IItemHandler itemHandler){
+        boolean justExtracted;
+        for (int i = 0; i < itemHandler.getSlots(); i++ ){
+            IEnergyStorage energyStorage = itemHandler.getStackInSlot(i).getCapability(Capabilities.EnergyStorage.ITEM);
+            justExtracted = extractPowerTo(energyStorage);
+            if (justExtracted){
+                return true;
             }
         }
+        return false;
+    }
+    public boolean forItemInStorage (Container itemStorage){
+        boolean justExtracted;
+        for (int i = 0; i < itemStorage.getContainerSize(); i++ ){
+            IEnergyStorage energyStorage = itemStorage.getItem(i).getCapability(Capabilities.EnergyStorage.ITEM);
+            justExtracted = extractPowerTo(energyStorage);
+            if (justExtracted){
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean extractPowerTo (IEnergyStorage energyStorage){
